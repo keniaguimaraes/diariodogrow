9a82c3105ea6959fa976aa61e94fc5014343ac45
Ubuntu <ubuntu@ip-172-31-42-10.ec2.internal>
2020-01-31 21:16:43 +0000

    Corrige configuração.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,9 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
-  config.serve_static_assets = false
+
+  
+config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -14,6 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -21,7 +23,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-config.assets.compress = true
+
 
 # Don't fallback to assets pipeline if a precompiled asset is missed  
 config.assets.compile = false
